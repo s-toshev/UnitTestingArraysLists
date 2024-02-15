1e18cf7b57db1f24cf7bea5bd8b1792cc25c979b
s-toshev <simeon.toshev@yahoo.bg>
2024-02-15 07:30:32 +0200

    Add project files.

---
+++ TestApp.UnitTests/AverageTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class AverageTests
+{
+    // TODO: finish the test
+    [Test]
+    public void Test_CalculateAverage_InputHasOneElement_ShouldReturnSameElement()
+    {
+        // Arrange
+        int[] array = { 42 };
+
+        // Act
+        double result;
+
+        // Assert
+        //Assert.That(result, Is.EqualTo(42));
+    }
+
+    [Test]
+    public void Test_CalculateAverage_InputHasPositiveIntegers_ShouldReturnCorrectAverage()
+    {
+        // TODO: finish the test
+    }
+
+    [Test]
+    public void Test_CalculateAverage_InputHasNegativeIntegers_ShouldReturnCorrectAverage()
+    {
+        // TODO: finish the test
+    }
+
+    [Test]
+    public void Test_CalculateAverage_InputHasMixedIntegers_ShouldReturnCorrectAverage()
+    {
+        // TODO: finish the test
+    }
+}
---
+++ TestApp/AdjacentEqual.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestApp;
+
+public class AdjacentEqual
+{
+    public static string Sum(List<int>? numbers)
+    {
+       for (int i = 0; i < numbers.Count - 1; i++)
+        {
+            if (numbers[i] != numbers[i + 1])
+            {
+                continue;
+            }
+
+            numbers[i] += numbers[i + 1];
+            numbers.RemoveAt(i + 1);
+            i = -1;
+        }
+
+        return string.Join(" ", numbers);
+    }
+}
